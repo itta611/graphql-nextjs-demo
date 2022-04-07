98fda01352c3e48acf490f49310bfc4ba944c681
Itta <86896657+itta611@users.noreply.github.com>
2022-04-07 17:34:05 +0900

    create graphql endpoint

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,7 @@
-import Head from 'next/head';
-import { Container, Text } from '@chakra-ui/react';
+import { Container } from '@chakra-ui/react';
 
-export default function Home() {
-  return (
-    <Container maxW={'5xl'}>
-      <Head>
-        <title>Create Next App</title>
-        <meta name='description' content='GraphQL demo' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Text>Hello, World!</Text>
-    </Container>
-  );
+function Index(props) {
+  return <Container maxW={'5xl'}>Hello, world</Container>;
 }
+
+export default Index;
